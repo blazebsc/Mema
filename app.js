e6a58dd6349527044fb0910de32e37dae7199f61
blazebsc <blakeisfruity@proton.me>
2024-12-29 14:52:55 +1300

    First Commit
    The Base of the Mema Site

---
+++ app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Mema Landing Page Loaded!');
+    
+    // Example Event Handler
+    document.querySelector('.cta-btn').addEventListener('click', () => {
+      alert('Thank you for joining Mema!');
+    });
+  });
+  
