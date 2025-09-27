e781b85bf2888b2cb25048caf72c9c4378ad16da
blazebsc <blakeisfruity@proton.me>
2025-09-27 15:58:34 +1200

    plz work

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -15,7 +15,7 @@ import {
   Palette
 } from 'lucide-react'
 import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '../assets/vite.svg'
 
 const features = [
   {
